a4a4b08cc77eebdf514695769b81941169d70468
ZhangTianyou602 <3638598929@qq.com>
2025-12-02 08:06:18 +0000

    fn main() {     call_me(); }
    fn call_me() {
        // 打印 5 次 "Hello, world!"，符合 Rustlings 该练习的默认要求
        for _ in 0..5 {
            println!("Hello, world!");
        }
    }

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,13 +1,10 @@
-// functions1.rs
-//
-// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a
-// hint.
-
-
 fn main() {
     call_me();
 }
 
 fn call_me() {
-
+    // 打印 5 次 "Hello, world!"，符合 Rustlings 该练习的默认要求
+    for _ in 0..5 {
+        println!("Hello, world!");
+    }
 }
