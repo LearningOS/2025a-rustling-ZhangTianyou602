aa944e9cd4a4391fef2e94b9f5f861b60daf9b19
ZhangTianyou602 <3638598929@qq.com>
2025-12-02 08:07:42 +0000

    fn main() {     call_me(3); }
    // 显式标注返回类型为 ()（无返回值），让代码更清晰规范
    fn call_me(num: i32) -> () {
        for i in 0..num {
            println!("Ring! Call number {}", i + 1);
        }
    }

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -1,14 +1,9 @@
-// functions2.rs
-//
-// Execute `rustlings hint functions2` or use the `hint` watch subcommand for a
-// hint.
-
-
 fn main() {
     call_me(3);
 }
 
-fn call_me(num: i32) {
+// 显式标注返回类型为 ()（无返回值），让代码更清晰规范
+fn call_me(num: i32) -> () {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
