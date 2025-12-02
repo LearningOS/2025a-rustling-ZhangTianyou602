b9ac53c7e314af85c5e9bf73a0f09a7d2636be6e
ZhangTianyou602 <3638598929@qq.com>
2025-12-02 08:11:04 +0000

    fn main() {     // 明确 Vec 存储的类型是 &str（字符串切片），匹配 push 的 "milk" 类型     let mut shopping_list: Vec<&str> = Vec::new();     shopping_list.push("milk"); }

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -1,14 +1,5 @@
-// generics1.rs
-//
-// This shopping list program isn't compiling! Use your knowledge of generics to
-// fix it.
-//
-// Execute `rustlings hint generics1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 fn main() {
-    let mut shopping_list: Vec<?> = Vec::new();
+    // 明确 Vec 存储的类型是 &str（字符串切片），匹配 push 的 "milk" 类型
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
