bf7ba84371e67a0afa1e38c910160bca34dc2d86
ZhangTianyou602 <3638598929@qq.com>
2025-12-10 11:50:27 +0000

    // move_semantics5.rs fn main() {     let mut x = 100;     let y = &mut x;     *y += 100;     // 关键重排：将 z 的定义和操作移到 y 的可变引用使用完毕后     let z = &mut x;     *z += 1000;     assert_eq!(x, 1200); }

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -1,16 +1,9 @@
 // move_semantics5.rs
-//
-// Make me compile only by reordering the lines in `main()`, but without adding,
-// changing or removing any of them.
-//
-// Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
-// for a hint.
-
-
 fn main() {
     let mut x = 100;
     let y = &mut x;
     *y += 100;
+    // 关键重排：将 z 的定义和操作移到 y 的可变引用使用完毕后
     let z = &mut x;
     *z += 1000;
     assert_eq!(x, 1200);
