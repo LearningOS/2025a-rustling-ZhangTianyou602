79cdadc4887ef300c653393f4a1330834029a88e
ZhangTianyou602 <3638598929@qq.com>
2025-12-10 11:37:57 +0000

    // modules1.rs mod sausage_factory {     // Don't let anybody outside of this module see this!     fn get_secret_recipe() -> String {         String::from("Ginger")     }
        // 关键修改：添加 pub 修饰符，让外部能调用该函数
        pub fn make_sausage() {
            get_secret_recipe();
            println!("sausage!");
        }
    }
    
    fn main() {
        sausage_factory::make_sausage();
    }

--- exercises/modules/modules1.rs
+++ exercises/modules/modules1.rs
@@ -1,17 +1,12 @@
 // modules1.rs
-//
-// Execute `rustlings hint modules1` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
 mod sausage_factory {
     // Don't let anybody outside of this module see this!
     fn get_secret_recipe() -> String {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    // 关键修改：添加 pub 修饰符，让外部能调用该函数
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
