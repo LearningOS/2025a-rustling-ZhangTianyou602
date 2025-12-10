af080985975e403e799d38735121dd38a33b5f2d
ZhangTianyou602 <3638598929@qq.com>
2025-12-10 11:36:13 +0000

    // macros3.rs // 关键：给模块添加 #[macro_use] 注解，暴露内部定义的宏 #[macro_use] mod macros {     macro_rules! my_macro {         () => {             println!("Check out my macro!");         };     } }
    fn main() {
        my_macro!();
    }

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -1,12 +1,6 @@
 // macros3.rs
-//
-// Make me compile, without taking the macro out of the module!
-//
-// Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
-
+// 关键：给模块添加 #[macro_use] 注解，暴露内部定义的宏
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
