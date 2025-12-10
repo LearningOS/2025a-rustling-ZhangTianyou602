8622f42c5795935b7f5b0d835dfa08e0ff822ba9
ZhangTianyou602 <3638598929@qq.com>
2025-12-10 11:24:21 +0000

    // intro2.rs // Make the code print a greeting to the world.
    fn main() {
        // 核心：使用 println! 宏输出 "Hello world!"
        println!("Hello world!");
    }

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -1,11 +1,7 @@
 // intro2.rs
-//
 // Make the code print a greeting to the world.
-//
-// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
-// hint.
-
 
 fn main() {
+    // 核心：使用 println! 宏输出 "Hello world!"
     println!("Hello world!");
 }
