4826923552d4cc3fbe370b00519455042b8184a5
ZhangTianyou602 <3638598929@qq.com>
2025-12-13 12:23:45 +0000

    fn main() {     let x = 9; // 改为非10的值     if x == 10 {         println!("x is ten!");     } else {         println!("x is not ten!"); // 此时会输出这行     } }

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -1,14 +1,8 @@
-// variables2.rs
-//
-// Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
-// hint.
-
-
 fn main() {
-    let x = 10;
+    let x = 9; // 改为非10的值
     if x == 10 {
         println!("x is ten!");
     } else {
-        println!("x is not ten!");
+        println!("x is not ten!"); // 此时会输出这行
     }
 }
